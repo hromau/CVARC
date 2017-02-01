1fd8517959f8a938d4206aeb2623d7ec7646b720
FoKycHuK <fokychuk47@ya.ru>
2017-02-01 22:45:00 +0500

    пока левый плей сервер и клиент

---
+++ PudgeClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CVARC.V2;
+using Pudge;
+using Pudge.World;
+
+namespace PudgeClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new PudgeClientLevel3();
+            client.Configurate(14000, new ConfigurationProposal(), new PudgeWorldState(123));
+
+            client.Move(10);
+            client.Hook();
+        }
+    }
+}
---
+++ uCVARC/Assets/Servers/PlayServer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using CVARC.V2;
+using Infrastructure;
+
+namespace Assets.Servers
+{
+    public class PlayServer : IDisposable
+    {
+        private TcpListener listener;
+        private TcpClient proxyConnection;
+        private bool gameStarted;
+        private Dictionary<string, TcpClient> players;
+
+        public PlayServer(int port)
+        {
+            listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+        }
+
+        public Dictionary<string, TcpClient> CheckGame()
+        {
+            if (!listener.Pending() || gameStarted)
+                return null;
+            proxyConnection = listener.AcceptTcpClient();
+            var gameSettings = proxyConnection.ReadJson<GameSettings>();
+            players = new Dictionary<string, TcpClient>();
+                
+            foreach (var settings in gameSettings.ActorSettings.Where(x => !x.IsBot))
+                players[settings.ControllerId] = listener.AcceptTcpClient();
+
+            gameStarted = true;
+
+            return players;
+        }
+
+        public void EndGame(GameResult gameResult)
+        {
+            proxyConnection.WriteJson(gameResult);
+            proxyConnection.Close();
+            foreach (var player in players.Values)
+                player.Close();
+            gameStarted = false;
+        }
+
+        public void Dispose()
+        {
+            listener.Stop();
+        }
+    }
+}

fd375926981a2d49dc9840ee47ec77e8c9789bb6
niemandkun <niemandkun@yandex.ru>
2017-02-07 12:56:19 +0500

    moar sensor data

---
+++ Competitions/Homm/HommCore/Sensors/DeathSensor.cs
@@ -0,0 +1,13 @@
+﻿using CVARC.V2;
+using HoMM.Robot;
+
+namespace HoMM.Sensors
+{
+    public class DeathSensor : Sensor<bool, IHommRobot>
+    {
+        public override bool Measure()
+        {
+            return Actor.IsDisabled;
+        }
+    }
+}

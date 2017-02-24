621e950f8516e5c0ba33d35e8fe51b7e1defa6a0
FoKycHuK <zagnoiko@skbkontur.ru>
2017-02-24 23:15:28 +0500

    обновления

--- Commons/SingleplayerProxy/SingleplayerProxyConfigurations.cs
+++ Commons/SingleplayerProxy/SingleplayerProxyConfigurations.cs
@@ -1,16 +1,17 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 
 namespace SingleplayerProxy
 {
     public static class SingleplayerProxyConfigurations
     {
         public static bool DebugMode = false; // do not check and do not start unity
-        public const bool UpdateEnabled = false;
+        public const bool UpdateEnabled = true;
         public const string PathToVersionFile = "version";
-        public const string PathToUnityDir = "";
-        public const string UnityExePath = PathToUnityDir + "ucvarc.exe";
-        public const string UrlToGetVersion = "url";
-        public const string UrlToGetUpdate = "url";
+        public static readonly string PathToUnityDir = AppDomain.CurrentDomain.BaseDirectory;
+        public const string UnityExePath = "ucvarc.exe";
+        public const string UrlToGetVersion = "http://homm.ulearn.me/Service/GetVersion";
+        public const string UrlToGetUpdate = "http://homm.ulearn.me/Service/GetUpdate";
         
         public static readonly IPEndPoint ProxyEndPoint = new IPEndPoint(IPAddress.Any, 18700);
         public static readonly IPEndPoint UnityEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 15000);

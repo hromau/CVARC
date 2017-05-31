1c03c66d9413f91eab0d76da5189d31fb1f67111
niemandkun <niemandkun@yandex.ru>
2017-05-31 17:10:38 +0500

    + tests for armies and scouting

--- Competitions/Homm/Homm.IntegrationTests/RealisticMapSensorScoutingTest.cs
+++ Competitions/Homm/Homm.IntegrationTests/RealisticMapSensorScoutingTest.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using FluentAssertions;
+using HoMM;
+using HoMM.ClientClasses;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,91 @@ using System.Threading.Tasks;
 
 namespace Homm.IntegrationTests
 {
-    class RealisticMapSensorScoutingTest
+    [TestFixture]
+    public class RealisticMapSensorScoutingTest
     {
+        private HommFinalLevelClient client;
+
+        [SetUp]
+        public void RunBeforeAnyTest()
+        {
+            client = new HommFinalLevelClient();
+            client.Configurate("127.0.0.1", 18700, Guid.Empty, operationalTimeLimit: 5, debugMap: true);
+        }
+
+        [Test]
+        public void Scouting_FarMapCorner_ShouldNotBeVisible_WithoutScouting()
+        {
+            var data = client.Wait(0.1);
+
+            data.Map.Objects
+                .Where(x => x.Location.ToLocation().EuclideanDistance(Location.Zero) > HommRules.Current.HeroViewRadius)
+                .Should().BeEmpty();
+
+            data.Map.Objects
+                .Should().HaveCount(24);
+        }
+
+        [Test]
+        public void Scouting_OtherHero_ShouldNotBeVisible_WithoutScouting()
+        {
+            var data = client.Wait(0.1);
+
+            data.Map.Objects
+                .Where(x => x.Hero != null)
+                .Should().HaveCount(1);
+
+            data.Map.Objects.Single(x => x.Hero != null).Hero.Name.Should().Be("Left");
+        }
+
+        [Test]
+        public void Scouting_FarMapCorner_ShouldBeVisible_WhenScoutingTile()
+        {
+            var data = client.ScoutTile(new LocationInfo(7, 7));
+
+            data.Map.Objects
+                .Where(x => x.Location.ToLocation().EuclideanDistance(Location.Zero) > HommRules.Current.HeroViewRadius)
+                .Should().HaveCount(6);
+
+            data.Map.Objects
+                .Where(x => x.Terrain == Terrain.Desert)
+                .Should().HaveCount(1);
+
+            data.Map.Objects
+                .Where(x => x.Terrain == Terrain.Marsh)
+                .Should().HaveCount(5);
+        }
+
+        [Test]
+        public void Scouting_OtherHero_ShouldBeVisible_WhenScoutingTile_IfInsideScoutingRadius()
+        {
+            var data = client.ScoutTile(new LocationInfo(7, 7));
+
+            data.Map.Objects
+                .Where(x => x.Location.ToLocation().EuclideanDistance(Location.Zero) > HommRules.Current.HeroViewRadius)
+                .Where(x => x.Hero != null)
+                .Should().NotBeEmpty();
+
+            data.Map.Objects
+                .Where(x => x.Location.ToLocation().EuclideanDistance(Location.Zero) > HommRules.Current.HeroViewRadius)
+                .Single(x => x.Hero != null)
+                .Hero.Name.Should().Be("Right");
+        }
+
+        [Test]
+        public void Scouting_OtherHero_ShouldBeVisible_WhenScoutingHero()
+        {
+            var data = client.ScoutHero();
+
+            data.Map.Objects
+                .Where(x => x.Location.ToLocation().EuclideanDistance(Location.Zero) > HommRules.Current.HeroViewRadius)
+                .Where(x => x.Hero != null)
+                .Should().NotBeEmpty();
+
+            data.Map.Objects
+                .Where(x => x.Location.ToLocation().EuclideanDistance(Location.Zero) > HommRules.Current.HeroViewRadius)
+                .Single(x => x.Hero != null)
+                .Hero.Name.Should().Be("Right");
+        }
     }
 }
--- Competitions/Homm/Homm.IntegrationTests/ScoutingGameplayTest.cs
+++ Competitions/Homm/Homm.IntegrationTests/ScoutingGameplayTest.cs
@@ -1,12 +1,98 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using CVARC.V2;
+using FluentAssertions;
+using HoMM;
+using HoMM.ClientClasses;
+using NUnit.Framework;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Homm.IntegrationTests
 {
+    [TestFixture]
     class ScoutingGameplayTest
     {
+        private HommFinalLevelClient client;
+
+        [SetUp]
+        public void RunBeforeAnyTest()
+        {
+            client = new HommFinalLevelClient();
+            client.Configurate("127.0.0.1", 18700, Guid.Empty, operationalTimeLimit: 5, debugMap: true);
+        }
+
+        [Test]
+        public void Scouting_ShouldNotSeeScoutedTiles_OnNextTurn()
+        {
+            client.ScoutTile(new LocationInfo(7, 7));
+            var data = client.Wait(0.1);
+
+            data.Map.Objects
+                .Where(x => x.Location.ToLocation().EuclideanDistance(Location.Zero) > HommRules.Current.HeroViewRadius)
+                .Should().BeEmpty();
+        }
+
+        [Test]
+        public void Scouting_ShouldNotSeeScoutedHero_OnNextTurn()
+        {
+            client.ScoutHero();
+            var data = client.Wait(0.1);
+
+            data.Map.Objects
+                .FirstOrDefault(x => x.Hero != null && x.Hero.Name == "Right")
+                .Should().BeNull();
+        }
+
+        [Test]
+        public void Scouting_ShouldThrowException_IfScoutingHeroOnCooldown()
+        {
+            client.ScoutHero();
+
+            Action scout = () => client.ScoutHero();
+            scout.ShouldThrow<ClientException>();
+        }
+
+        [Test]
+        public void Scouting_ShouldThrowException_IfScoutingTileOnCooldown()
+        {
+            client.ScoutTile(new LocationInfo(0, 0));
+
+            Action scout = () => client.ScoutTile(new LocationInfo(0, 0));
+            scout.ShouldThrow<ClientException>();
+        }
+
+        [Test]
+        public void Scouting_ShouldThrowException_IfScoutingTileAfterHeroCooldown()
+        {
+            client.ScoutHero();
+
+            Action scout = () => client.ScoutTile(new LocationInfo(0, 0));
+            scout.ShouldThrow<ClientException>();
+        }
+
+        [Test]
+        public void Scouting_ShouldThrowException_IfScoutingHeroAfterTileCooldown()
+        {
+            client.ScoutTile(new LocationInfo(0, 0));
+
+            Action scout = () => client.ScoutHero();
+            scout.ShouldThrow<ClientException>();
+        }
+
+        [Test]
+        public void Scouting_ShouldThrowException_WhenLocationIsOutOfMapBounds()
+        {
+            Action scout = () => client.ScoutTile(new LocationInfo(-1, -1));
+            scout.ShouldThrow<ClientException>();
+        }
+
+        [Test]
+        public void Scouting_ShouldBeAvailable_AfterCooldown()
+        {
+            client.ScoutHero();
+            client.Wait(HommRules.Current.ScoutingCooldown);
+
+            Action scout = () => client.ScoutHero();
+            scout.ShouldNotThrow();
+        }
     }
 }
